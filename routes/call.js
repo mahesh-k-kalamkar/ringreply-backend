15164d010f7ab04c7c0778058e629cf38eb617df
Admin <mkkalamkar5288@gmail.com>
2026-04-09 12:51:06 +0530

    Template Message Fix1

--- routes/call.js
+++ routes/call.js
@@ -7,6 +7,6 @@ const whatsappController = require('../controllers/whatsappController');
 router.use(auth);
 
 // Main endpoint for Android app (Call Detection)
-router.post('/sendCallReply', whatsappController.sendCallReply);
+router.post('api/sendCallReply', whatsappController.sendCallReply);
 
 module.exports = router;
